0e3b9152fc30b237f3de8fe936e6b2373aacfeca
Leonardo Omaña <leotangram@gmail.com>
2019-06-19 11:37:11 -0500

    Header and footer.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import AddProduct from './components/AddProduct'
 import Products from './components/Products'
 import EditProduct from './components/EditProduct'
 import Product from './components/Product'
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/new-product" component={AddProduct} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/products/:id" component={Product} />
-        <Route exact path="/products/edit/:id" component={EditProduct} />
-      </Switch>
+      <Header />
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/new-product" component={AddProduct} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/:id" component={Product} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </main>
+      <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
     </Router>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
+      <Link to="/products" className="navbar-brand">
+        React CRUD & Routing
+      </Link>
+    </div>
+  </nav>
+)
+
+export default Header
