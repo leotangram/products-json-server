f5103eea589202c229e647c60f75877c59746d07
Leonardo Omaña <leotangram@gmail.com>
2019-06-19 12:22:38 -0500

    Send result to products.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,12 @@ function App() {
       <Header />
       <main className="container mt-5">
         <Switch>
+          <Route
+            exact
+            path="/products"
+            render={() => <Products products={products} />}
+          />
           <Route exact path="/new-product" component={AddProduct} />
-          <Route exact path="/products" component={Products} />
           <Route exact path="/products/:id" component={Product} />
           <Route exact path="/products/edit/:id" component={EditProduct} />
         </Switch>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import ProductList from './ProductList'
 
-function Product() {
-  return <h1 className="text-center">Datos del producto</h1>
+function Product({ products }) {
+  return (
+    <Fragment>
+      <h1 className="text-center">Datos del producto</h1>
+      <ul className="list-group-mt-5">
+        {products.map(product => (
+          <ProductList key={product.id} product={product} />
+        ))}
+      </ul>
+    </Fragment>
+  )
 }
 
 export default Product
